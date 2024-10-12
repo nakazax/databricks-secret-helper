d8a0d95aadd6b5eec80865b12c414cbe2a80d81e
nakazax <nakazax>
2024-10-12 15:08:52 +0000

    Added notebook and README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Databricks Secret Helper
+This repository contains a Databricks notebook designed to assist with secret management in Databricks. It provides functionality for listing secret scopes, creating new scopes, and adding secrets.
---
+++ databricks_secret_helper.py
@@ -0,0 +1,66 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Databricks Secret Helper
+# MAGIC This notebook is designed for managing secrets in Databricks, including setting up secret scopes and keys.
+
+# COMMAND ----------
+
+from databricks.sdk import WorkspaceClient
+import pandas as pd
+
+w = WorkspaceClient()
+
+# COMMAND ----------
+
+dbutils.widgets.text("secret_scope", "hinak-secret-scope", "Name of Secret Scope")
+dbutils.widgets.text("secret_key", "", "Secret Key")
+dbutils.widgets.text("secret_value", "", "Secret Value")
+
+secret_scope = dbutils.widgets.get("secret_scope")
+secret_key = dbutils.widgets.get("secret_key")
+secret_value = dbutils.widgets.get("secret_value")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## List secret scopes
+
+# COMMAND ----------
+
+scope_list = []
+for scope in w.secrets.list_scopes():
+    scope_list.append({
+        "name": scope.name,
+        "backend_type": str(scope.backend_type)
+    })
+df_scopes = pd.DataFrame(scope_list)
+display(df_scopes)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Create secret scope if not exists
+
+# COMMAND ----------
+
+if secret_scope not in df_scopes['name'].values:
+    try:
+        w.secrets.create_scope(scope=secret_scope)
+        print(f"Secret scope '{secret_scope}' created successfully.")
+    except Exception as e:
+        print(f"Error creating secret scope: {str(e)}")
+else:
+    print(f"Secret scope '{secret_scope}' already exists.")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Put secret
+
+# COMMAND ----------
+
+try:
+    w.secrets.put_secret(scope=secret_scope, key=secret_key, string_value=secret_value)
+    print(f"Secret '{secret_key}' added to scope '{secret_scope}' successfully.")
+except Exception as e:
+    print(f"Error adding secret '{secret_key}' to scope '{secret_scope}': {str(e)}")
